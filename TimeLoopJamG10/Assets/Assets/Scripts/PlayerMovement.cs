4fd98edaae5dcd25346c83ea3817f3fbc7d9e5b5
SebastianM <mendozasebastian86@gmail.com>
2022-10-26 21:08:41 -0500

    Player animation
    Realize el blend tree y el script de movimiento del personaje.

---
+++ TimeLoopJamG10/Assets/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Rigidbody2D playerRb;
+    private Vector2 moveinput;
+    private Animator playerAnimator;
+   
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        //el void Update se utiliza para recibir Inputs del usuario.
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveinput = new Vector2(moveX, moveY).normalized;
+
+        playerAnimator.SetFloat("Horizontal", moveX);
+        playerAnimator.SetFloat("Vertical", moveY);
+        playerAnimator.SetFloat("speed", moveinput.sqrMagnitude);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //el void FixedUpdate se utiliza para simular fisicas, ya que toma el reloj interno del motor y no depende de los fotogramas del usuario.
+        playerRb.MovePosition(playerRb.position + moveinput * speed * Time.fixedDeltaTime);
+
+    }
+
+
+
+
+}
