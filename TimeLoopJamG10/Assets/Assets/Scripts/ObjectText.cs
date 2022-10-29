4a0ca1d1a3e3cc7979f0e60dc7309c3ec38e9b43
Kelly Escobar <111096067+kellyes93@users.noreply.github.com>
2022-10-28 23:15:00 -0500

    Looby listo
    Looby Listo, todo el escenario, colliders, animaciones, textos y sonidos

--- TimeLoopJamG10/Assets/Assets/Scripts/ObjectText.cs
+++ TimeLoopJamG10/Assets/Assets/Scripts/ObjectText.cs
@@ -1,15 +1,84 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ObjectText : MonoBehaviour
 {
     [SerializeField] private GameObject excMark;
+    [SerializeField, TextArea(4, 6)] private string[] ObjectLines;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text objectText;
+    [SerializeField] private float typingTime;
+
+
+    //[SerializeField] private float typingTime;
+
     private bool isPlayerInRange;
+    private bool didTextStart;
+    private int lineIndex;
+    
+   
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerInRange && Input.GetButtonDown("Fire1"))
+        {
+            if (!didTextStart)
+            {
+                StartText();
+            }
+            else if (objectText.text == ObjectLines[lineIndex])
+            {
+                NextLineText();
+            }
+            else
+            {
+                StopAllCoroutines();
+                objectText.text = ObjectLines[lineIndex];
+            }
+            
+        }
+    }
+
+    private void StartText()
+    {
+        didTextStart = true;
+        dialoguePanel.SetActive(true);
+        excMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextLineText()
+    {
+        lineIndex++;
+        if(lineIndex < ObjectLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didTextStart=false;
+            dialoguePanel.SetActive(false);
+            excMark.SetActive(true);
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        objectText.text = string.Empty;
+        int charIndex = 0;
+        foreach (char ch in ObjectLines[lineIndex])
+        {
+            objectText.text += ch;
+            charIndex++;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
